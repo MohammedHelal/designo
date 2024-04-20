519a247281a400371f9e81195b63eec8367de862
Mohammed Omar Helal <mohd.omar.helal@gmail.com>
2024-04-20 11:17:23 +0800

    adding tailwind and navbar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,19 @@
+import logo from "../assets/shared/desktop/logo-dark.png";
+//import logoWhite from "../assets/shared/desktop/logo-light.png";
+
 function Layout() {
   return (
     <>
-      <header></header>
+      <header>
+        <nav className="w-full px-6 py-12 flex flex-row justify-between items-center">
+          <img src={logo} alt="Logo header" />
+          <ul className="flex flex-row items-center">
+            <li className="px-6 cursor-pointer">Our Company</li>
+            <li className="px-6 cursor-pointer">Locations</li>
+            <li className="px-6 cursor-pointer">Contact</li>
+          </ul>
+        </nav>
+      </header>
       <footer></footer>
     </>
   );
