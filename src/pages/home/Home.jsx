3f79223f5b104dc0d5897af5a478c496bf7a5a49
Mohammed Omar Helal <mohd.omar.helal@gmail.com>
2024-04-26 22:48:45 +0800

    finishing 2nd section of home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
 import Layout from "../../layout/Layout";
 import hero from "../../assets/home/desktop/bg-pattern-hero-home.svg";
+import app from "../../assets/home/desktop/image-app-design.jpg";
+import graphic from "../../assets/home/desktop/image-graphic-design.jpg";
+import web from "../../assets/home/desktop/image-web-design-large.jpg";
 
 function Home() {
   return (
@@ -23,7 +26,43 @@ function Home() {
           alt="background hero"
         />
       </section>
-      <section></section>
+      <section className="flex justify-between w-full h-[640px] mb-32">
+        <div className="relative">
+          <img className="rounded-2xl" src={web} alt="Web Design Photo" />
+          <div className="bg-[#00000080] rounded-2xl absolute top-0 bottom-0 left-0 right-0 z-20 flex justify-center items-center">
+            <div className="text-white text-center">
+              <h2 className="my-6">WEB DESIGN</h2>
+              <p className="my-6 tracking-[5px]">
+                VIEW PROJECTS <span className="text-peach">{">"}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          <div className="relative">
+            <img className="rounded-2xl" src={app} alt="Web Design Photo" />
+            <div className="bg-[#00000080] rounded-2xl absolute top-0 bottom-0 left-0 right-0 z-20 flex justify-center items-center">
+              <div className="text-white text-center">
+                <h2 className="my-6">APP DESIGN</h2>
+                <p className="my-6 tracking-[5px]">
+                  VIEW PROJECTS <span className="text-peach">{">"}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <img className="rounded-2xl" src={graphic} alt="Web Design Photo" />
+            <div className="bg-[#00000080] rounded-2xl absolute top-0 bottom-0 left-0 right-0 z-20 flex justify-center items-center">
+              <div className="text-white text-center">
+                <h2 className="my-6">GRAPHIC DESIGN</h2>
+                <p className="my-6 tracking-[5px]">
+                  VIEW PROJECTS <span className="text-peach">{">"}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
