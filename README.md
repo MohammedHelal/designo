ff17dbb571d69d1aeb7004c1f30904c0af0ff462
Mohammed Omar Helal <mohd.omar.helal@gmail.com>
2024-05-11 10:55:11 +0800

    updating readme

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
-# React + Vite
+# Frontend Mentor - Designo agency website solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a solution to the [Designo agency website challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/designo-multipage-website-G48K6rfUT). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-Currently, two official plugins are available:
+## Table of contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements throughout the site
+- Receive an error message when the contact form is submitted if:
+  - The `Name`, `Email Address` or `Your Message` fields are empty should show "Can't be empty"
+  - The `Email Address` is not formatted correctly should show "Please use a valid email address"
+- **Bonus**: View actual locations on the locations page maps (we recommend [Leaflet JS](https://leafletjs.com/) for this)
+
+### Screenshot
+
+![Designo screenshot](./public/designo-screenshot.png)
+
+Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
+
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
+
+Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
+
+**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+
+### Links
+
+- Live Site URL: [Desgino site](https://mohammedhelal.github.io/designo)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
--- src/pages/design/design-array.js
+++ src/pages/design/design-array.js
@@ -1,91 +1,108 @@
+import web1 from "../../assets/web-design/desktop/image-express.jpg";
+import web2 from "../../assets/web-design/desktop/image-transfer.jpg";
+import web3 from "../../assets/web-design/desktop/image-photon.jpg";
+import web4 from "../../assets/web-design/desktop/image-blogr.jpg";
+import web5 from "../../assets/web-design/desktop/image-blogr.jpg";
+import web6 from "../../assets/web-design/desktop/image-camp.jpg";
+
 export const webGrid = [
   {
     id: "w01",
-    image: "./src/assets/web-design/desktop/image-express.jpg",
+    image: web1,
     title: "express",
     para: "A multi-carrier shipping website for ecommerce business",
   },
   {
     id: "w02",
-    image: "./src/assets/web-design/desktop/image-transfer.jpg",
+    image: web2,
     title: "transfer",
     para: "Site for low-cost money transfers and sending money within seconds",
   },
   {
     id: "w03",
-    image: "./src/assets/web-design/desktop/image-photon.jpg",
+    image: web3,
     title: "photon",
     para: "A state-of-the-art music player with high-resolution audio and DSP effects",
   },
   {
     id: "w04",
-    image: "./src/assets/web-design/desktop/image-builder.jpg",
+    image: web4,
     title: "builder",
     para: "Connects users with local contractors based on their location",
   },
   {
     id: "w05",
-    image: "./src/assets/web-design/desktop/image-blogr.jpg",
+    image: web5,
     title: "blogr",
     para: "Blogr is a platform for creating an online blog or publication",
   },
   {
     id: "w06",
-    image: "./src/assets/web-design/desktop/image-camp.jpg",
+    image: web6,
     title: "camp",
     para: "Get expert training in coding, data, design, and digital marketing",
   },
 ];
 
+import app1 from "../../assets/app-design/desktop/image-airfilter.jpg";
+import app2 from "../../assets/app-design/desktop/image-eyecam.jpg";
+import app3 from "../../assets/app-design/desktop/image-faceit.jpg";
+import app4 from "../../assets/app-design/desktop/image-todo.jpg";
+import app5 from "../../assets/app-design/desktop/image-loopstudios.jpg";
+
 export const appGrid = [
   {
     id: "a01",
-    image: "./src/assets/app-design/desktop/image-airfilter.jpg",
+    image: app1,
     title: "Airfilter",
     para: "Solving the problem of poor indoor air quality by filtering the air",
   },
   {
     id: "a02",
-    image: "./src/assets/app-design/desktop/image-eyecam.jpg",
+    image: app2,
     title: "Eyecam",
     para: "Product that lets you edit your favorite photos and videos at any time",
   },
   {
     id: "a03",
-    image: "./src/assets/app-design/desktop/image-faceit.jpg",
+    image: app3,
     title: "Faceit",
     para: "Get to meet your favorite internet superstar with the faceit app",
   },
   {
     id: "a04",
-    image: "./src/assets/app-design/desktop/image-todo.jpg",
+    image: app4,
     title: "Todo",
     para: "A todo app that features cloud sync with light and dark mode",
   },
   {
     id: "a05",
-    image: "./src/assets/app-design/desktop/image-loopstudios.jpg",
+    image: app5,
     title: "Loopstudios",
     para: "A VR experience app made for Loopstudios",
   },
 ];
 
+import graphic1 from "../../assets/graphic-design/desktop/image-change.jpg";
+import graphic2 from "../../assets/graphic-design/desktop/image-boxed-water.jpg";
+import graphic3 from "../../assets/graphic-design/desktop/image-science.jpg";
+
 export const graphicGrid = [
   {
     id: "g01",
-    image: "./src/assets/graphic-design/desktop/image-change.jpg",
+    image: graphic1,
     title: "Tim Brown",
     para: "A book cover designed for Tim Brown’s new release, ‘Change’",
   },
   {
     id: "g02",
-    image: "./src/assets/graphic-design/desktop/image-boxed-water.jpg",
+    image: graphic2,
     title: "Boxed water",
     para: "A simple packaging concept made for Boxed Water",
   },
   {
     id: "g03",
-    image: "./src/assets/graphic-design/desktop/image-science.jpg",
+    image: graphic3,
     title: "Science!",
     para: "A poster made in collaboration with the Federal Art Project",
   },
